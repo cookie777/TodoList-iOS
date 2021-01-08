f6c8437cca81b810e4d2cbf2b3efa716ed3af811
cookie777 <l.oeil.ballon@gmail.com>
2021-01-07 22:45:59 -0800

    add: initial files

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Takayuki Yamaguchi on 2021-01-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
